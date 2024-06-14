935b11f5b9fa3c0eb87b470f89a3cc798a21ef21
Nina Burgdorfer <nina.burgdorfer@meteoswiss.ch>
2024-06-14 20:57:07 +0200

    Refactor pre-process app script

--- app/app.py
+++ app/app.py
@@ -1,188 +1,183 @@
-"""
-Process unprocessed IFS data using meteodatalab.
-Author: Nina Burgdorfer
-Date: 03.04.2024
-"""
-import logging  
+"""Pre-Process IFS HRES data as input to Flexpart."""
+import logging
 import tempfile
-import json
+import os
 from pathlib import Path
-import time
+from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
-from boto3.s3.transfer import TransferConfig 
-import subprocess
-import os
-import eccodes
+from boto3.s3.transfer import TransferConfig
 import numpy as np
-import xarray as xr
-import yaml
-from importlib.resources import files
-import sys
-import earthkit.data
-
-
-# First-party
 import meteodatalab.operators.flexpart as flx
-from meteodatalab import grib_decoder
+from meteodatalab import grib_decoder, data_source, metadata, config
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# Initialize logging
+logging.basicConfig(level=logging.INFO)
 
-# Set the desired multipart threshold value (5GB)
+# Configuration
 GB = 1024 ** 3
-config = TransferConfig(multipart_threshold=5*GB)
+TRANSFER_CONFIG = TransferConfig(multipart_threshold=5 * GB)
+S3INPUT_ENDPOINT_URL = "https://object-store.os-api.cci1.ecmwf.int"
+S3OUTPUT_ENDPOINT_URL = "" # NB: doesn't exist yet  
+S3INPUT_BUCKET_NAME = "flexpart-input"
+S3OUTPUT_BUCKET_NAME = "flexpart-output" # NB: doesn't exist yet  
+TINCR = int(os.getenv('TINCR', 3))
+TSTART = int(os.getenv('TSTART', 0))
 
-# Initializing variables for the S3 client
-S3_BUCKET_NAME = "flexpart-input"
-S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
-S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
-S3_ENDPOINT_URL = "https://object-store.os-api.cci1.ecmwf.int"
-
-def download_ecmwf(file, location, info=''):
-    
-    logging.info(f"Downloading {info} {file} from S3 {S3_BUCKET_NAME} bucket")
-
-    # Creating an S3 client to interact with S3 bucket 
-    s3_client = boto3.client(
-    's3',
-    endpoint_url=S3_ENDPOINT_URL,
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY,
-    use_ssl=True,
+def get_s3_client(endpoint_url, access_key, secret_key):
+    return boto3.client(
+        's3',
+        endpoint_url=endpoint_url,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        use_ssl=True,
     )
-    
-    try: 
-        start = time.time()
-        response = s3_client.download_file(S3_BUCKET_NAME, Path(file).name, location, Config=config)
-        elapsed = time.time() - start
-        logging.info(f"Finished downloading {info} {file} in: {elapsed} seconds.")
 
-    except Exception as e:
-        raise(e)
-    
+s3_input = get_s3_client(S3INPUT_ENDPOINT_URL, os.getenv('S3INPUT_ACCESS_KEY'), os.getenv('S3INPUT_SECRET_KEY'))
+# NB: doesn't exist yet  
+# s3_output = get_s3_client(S3OUTPUT_ENDPOINT_URL, os.getenv('S3OUTPUT_ACCESS_KEY'), os.getenv('S3OUTPUT_SECRET_KEY'))
 
-
-def list_objects_from_s3():
-    
-    logging.debug(locals())
-
-    logging.info("List objects inside our bucket")
-    
-    # Creating an S3 client to interact with S3 bucket 
-    s3_client = boto3.client(
-    's3',
-    endpoint_url=S3_ENDPOINT_URL,
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY,
-    use_ssl=True,
-    )
-    logging.info("Connect with bucket")
-    
+def download_file(s3_client, bucket, key, local_path):
     try:
-        response = s3_client.head_bucket(Bucket=S3_BUCKET_NAME)
-        logging.info("Connection successful. Bucket exists.")
-    except Exception as e:
-        logging.error("Connection failed: %s", e)
+        s3_client.download_file(bucket, key, local_path, Config=TRANSFER_CONFIG)
+        logging.info(f"Downloaded file from S3: {key}")
+    except ClientError as e:
+        logging.error(f"Error downloading file {key}: {e}")
+
+def upload_file(s3_client, bucket, local_path):
+    key = os.path.basename(local_path)
+    try:
+        s3_client.upload_file(local_path, bucket, key)
+        logging.info(f"Uploaded file to S3: {key}")
+    except ClientError as e:
+        logging.error(f"Error uploading file {local_path}: {e}")
+
+def validate_dataset(ds, params, ref_time, step, prev_step):
+    if not all(param in ds.keys() for param in params):
+        raise ValueError("Not all requested parameters are present in the dataset")
+    if not all(
+        (np.array_equal(array.coords["time"].values, [0])) or
+        (prev_step == 0 and np.array_equal(array.coords["time"].values, [0, step])) or
+        (prev_step != 0 and np.array_equal(array.coords["time"].values, [0, prev_step, step]))
+        for array in ds.values()
+    ):
+        raise ValueError("Downloaded steps are incorrect")
+    if not all((array.coords["ref_time"].values == np.datetime64(ref_time, 'ns')) for array in ds.values()):
+        raise ValueError("The forecast reference time is incorrect")
+
+def process_fields(ds_out, ds_in, input_fields, constant_fields):
+    missing_fields = [field for field in ds_in.keys() if field not in ds_out.keys() and field in input_fields and field != 'etadot']
+    missing_const = [const for const in ds_in.keys() if const not in ds_out.keys() and const in constant_fields]
+
+    for field in ds_out:
+        ds_out[field] = ds_out[field].isel(time=slice(-1, None)).squeeze()
+    for field in missing_fields:
+        logging.warning(f"Field '{field}' not found in output")
+        ds_out[field] = ds_in[field].isel(time=slice(-1, None)).squeeze()
+    for field in missing_const:
+        ds_out[field] = ds_in[field].squeeze()
+
+    ds_out['etadot'] = ds_out.pop('omega')
+    ds_out['cp'] = ds_out['cp'] * 1000
+    ds_out['lsp'] = ds_out['lsp'] * 100
+
+def pre_process(file_objs):
+    def download_temp_file(file_info):
+        temp_file = tempfile.NamedTemporaryFile(suffix=file_info['key'], delete=False)
+        download_file(s3_input, S3INPUT_BUCKET_NAME, file_info['key'], temp_file.name)
+        file_info['temp_file'] = temp_file.name
+        return temp_file
+
+    file_objs.sort(key=lambda x: int(x['step']), reverse=True)
+    if len(file_objs) < 3:
+        raise ValueError("Not enough files for pre-processing")
+
+    to_process = file_objs[0]
+    step_to_process = int(to_process['step'])
+    forecast_ref_time = to_process['forecast_ref_time']
+
+    if to_process['processed'] == "Y":
+        logging.warning(f"Already processed timestep {step_to_process}. Skipping.")
         return
 
-    objects = s3_client.list_objects_v2(Bucket=S3_BUCKET_NAME)
+    temp_files = [download_temp_file(to_process)]
+    prev_file = file_objs[1]
+    prev_step = int(prev_file['step'])
 
-    logging.info("List content of the bucket")
-    # Check if 'Contents' key exists in objects
-    if 'Contents' in objects:
-        # Extract and print the 'Contents' key
-        contents = objects['Contents']
-        logging.info("Contents:")
-        for item in contents:
-            key = item.get('Key', 'Unknown Key')
-            logging.info(f"Key: {key}")
+    if prev_step != 0:
+        temp_files.append(download_temp_file(prev_file))
+        init_files = file_objs[2:4]
     else:
-        logging.info("Bucket is empty or 'Contents' key does not exist.")
+        init_files = file_objs[1:3]
 
+    init_files.sort(key=lambda x: x['key'][-2:])
+    temp_files.extend([download_temp_file(f) for f in init_files])
 
-def upload_file(file_name, object_name=None):
-    """Upload a file to the S3 bucket
+    datafiles = [f.name for f in temp_files]
 
-    :param file_name: File to upload
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
+    request = {"param": list(input_fields + constants)}
+    with config.set_values(data_scope="ifs"):
+        source = data_source.DataSource(datafiles=datafiles)
+        ds_in = grib_decoder.load(source, request)
+        validate_dataset(ds_in, request["param"], forecast_ref_time, step_to_process, prev_step)
+        ds_in |= metadata.extract_pv(ds_in["u"].message)
 
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
+    ds_out = flx.fflexpart(ds_in)
+    process_fields(ds_out, ds_in, input_fields, constants)
 
-    # Upload the file
-    s3_client = boto3.client(
-    's3',
-    endpoint_url=S3_ENDPOINT_URL,
-    aws_access_key_id=S3_ACCESS_KEY,
-    aws_secret_access_key=S3_SECRET_KEY,
-    use_ssl=True,
-    )
-    try:
-        response = s3_client.upload_file(file_name, S3_BUCKET_NAME, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
+    for temp_file in temp_files:
+        temp_file.close()
 
-def main():
-    # Check the content of the S3 before uploading files 
-    list_objects_from_s3()
+    output_file = tempfile.NamedTemporaryFile(suffix=f"output_dispf{forecast_ref_time}{step_to_process}", delete=False)
+    with open(output_file.name, "wb") as fout:
+        for name, field in ds_out.items():
+            logging.info(f"Writing GRIB fields to {output_file.name}")
+            grib_decoder.save(field, fout)
 
-    # Upload files to s3
-    should_upload_to_s3 = False
+    # NB: s3-output doesn't exist yet 
+    # upload_file(s3_output, S3OUTPUT_BUCKET_NAME, output_file.name)
 
-    # Select 4 timesteps to upload (min. 3)
-    # NB: Atm. this is hardcoded but later event should be triggered when ecmwf files arrived in s3
-    # Location in the container to upload from to s3
-    folder_path = "/data/24040100"
-    datafiles = ["efsf00000000", "efsf00010000", "efsf00020000", "efsf00030000"]
+def select_objects(objects):
+    obj_in_s3 = []
 
-    if should_upload_to_s3:
-        # Upload files
-        for file in datafiles:
-            # Upload the file
-            upload_success = upload_file(os.path.join(folder_path, file))
-            
-            # Check if the file was uploaded successfully
-            if upload_success:
-                logging.info(f"File {file} uploaded successfully!")
-            else:
-                logging.info(f"Failed to upload file {file}.")
-    else:
-        logging.info("Skipping file upload as they are aleady uploaded.")
-    
-    # Check the content of the S3 after uploading files 
-    list_objects_from_s3()
+    for item in objects.get('Contents', []):
+        key = item.get('Key')
+        forecast_ref_time = datetime.strptime(key[3:11], "%Y%m%d%H")
+        valid_time_str = key[11:18]
+        valid_time_obj = datetime.strptime(valid_time_str, "%y%m%d%H")
 
-    # meteodata-lab job
-    # Constants and input fields keys
-    constants = ("z", "lsm", "sdor")
-    inputf = (
-        "etadot", "t", "q", "u", "v", "sp", "10u", "10v", "2t", "2d", "tcc", "sd",
-        "cp", "lsp", "ssr", "sshf", "ewss", "nsss"
-    )
+        step = 0 if valid_time_obj.minute == 1 else int((valid_time_obj - forecast_ref_time).total_seconds() / 3600)
+        obj_in_s3.append({
+            "key": key,
+            "forecast_ref_time": forecast_ref_time,
+            "step": step,
+            "processed": "N",
+        })
 
-    valid_temp_files = []
+    obj_in_s3.sort(key=lambda x: x['forecast_ref_time'])
+    for fcst_ref_time, group in groupby(obj_in_s3, key=lambda x: x['forecast_ref_time']):
+        files_per_run = list(group)
+        step_zero = [file for file in files_per_run if file['step'] == 0]
+        steps = [file['step'] for file in files_per_run]
 
-    # Download from S3
-    for index, file in enumerate(datafiles):
-        timestep = index + 1  # Timestep starts from 1
-        # Create a temporary file with a name that includes the index
+        if len(step_zero) < 2:
+            logging.info(f"Currently only {len(step_zero)} step=0 files are available. Waiting for these before processing.")
+            continue
 
-        with tempfile.NamedTemporaryFile(suffix=f'_file_{index}', delete=False) as tmp_file:
-            # Download the file from S3
-            download_ecmwf(file, tmp_file.name, info=f'timestep {timestep}')
-            valid_temp_files.append(tmp_file.name)
-
-    """
-    ds = grib_decoder.load_fieldnames(
-        constants + inputf, valid_temp_files, ref_param="T", extract_pv="U"
-    )
-    """
+        for file in files_per_run:
+            step = file['step']
+            if (step - TSTART) % TINCR == 0:
+                prev_step = step - TINCR
+                if prev_step in steps and file['processed'] == "N":
+                    logging.info(f"Launching Pre-Processing for timestep {step}")
+                    prev_file = next((item for item in files_per_run if item['step'] == prev_step), None)
+                    if prev_file:
+                        pre_process([file, prev_file, step_zero])
+                        file['processed'] = "Y"
+                else:
+                    logging.info(f"Not launching Pre-Processing for timestep {step}, {prev_step} in {steps}: {prev_step in steps}, processed: {file['processed'] == 'Y'}")
 
 if __name__ == "__main__":
-    main()
+    objects = s3_input.list_objects_v2(Bucket=S3INPUT_BUCKET_NAME)
+    # NB: atm. loop through all objects on s3 but next: only on new data 
+    select_objects(objects)
